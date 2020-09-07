a2b4f1fb1e16e424b53f39ce61e65960c8a61f0d
Swamita <swamita2001@gmail.com>
2020-09-07 13:48:56 +0530

    Initial Commit

---
+++ hotdogClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotdogClassifier
+//
+//  Created by Swamita on 07/09/20.
+//  Copyright © 2020 Swamita Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
